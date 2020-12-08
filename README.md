0a18b9ffdf6bd38f32d4c053bbacdeeb53d8f573
Dave Mackey <dave@davemackey.net>
2020-12-08 18:11:47 +0000

    Move query into basic_query, new multiple query

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 ## Requests
 This script relies on the Requests Library to do the heavy lifting. If you are unfamiliar with Requests, see below article:
 [https://realpython.com/python-requests/]
+
+## Visual Studio Code
+This project includes a .devcontainer folder which has a definition of a container that can be consumed by VSC to quickly spinup a dev environment.
+
+In addition, there is a .vscode folder which contains a settings.json file that overrides VSC's default settings. You'll need to update the pythonPath. Easiest way to accomplish this is: View --> Command Palette -> Python: Select Interpreter.
--- diigging.py
+++ diigging.py
@@ -6,19 +6,43 @@ API_AUTHORIZATION_HEADER = config('AUTHORIZATION')
 API_USER = config('USER')
 API_KEY = config('KEY')
 
-# Basic Query
-response = requests.get(
-    'https://secure.diigo.com/api/v2/bookmarks',
-    params={
-        'key': API_KEY,
-        'user': API_USER,
-        'count': 10,
-        'filter': 'all'
-    },
-    headers={
-        'Authorization': API_AUTHORIZATION_HEADER,
-    }
-)
+# Define some constant values
+request_url = 'https://secure.diigo.com/api/v2/bookmarks'
 
-json_response = response.json()
-print(json_response)
+# Basic Query Returns 10 Bookmarks
+def basic_query():
+    response = requests.get(
+        request_url,
+        params={
+            'key': API_KEY,
+            'user': API_USER,
+            'count': 10,
+            'filter': 'all'
+        },
+        headers={
+            'Authorization': API_AUTHORIZATION_HEADER,
+        }
+    )
+
+    json_response = response.json()
+    print(json_response)
+
+# Query Through Multiple Pages
+def query_multiple_pages():
+    page_number = 1
+    response = requests.get(
+        request_url,
+        params={
+            'key': API_KEY,
+            'user': API_USER,
+            'count': 10,
+            'filter': 'all'
+        },
+        headers={
+            'Authorization': API_AUTHORIZATION_HEADER,
+        }
+    )
+
+
+# Uncomment line below to run basic_query
+# basic_query()
