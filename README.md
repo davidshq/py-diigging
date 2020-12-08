7e73f354335ec462fe1790717f5d3a9b259dd4d9
Dave Mackey <dave@davemackey.net>
2020-12-08 19:09:17 +0000

    Add query_all_pages(), rename basic_query()

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Diigging: A Python Script for Interacting with the Diigo API
 
+## How To Use
+You'll need to rename example.env to .env and fill in the values for it's three variables before using the script.
+
+This script currently includes three functions: `query_basic()`, `query_multiple_pages()`, and `query_all_pages()`.
+
+At the bottom of the script are calls to each of the three functions. Two are commented by default, one uncommented. If you want to run a different query uncomment the desired function call and comment the unneeded function call.
 
 ## Requests
 This script relies on the Requests Library to do the heavy lifting. If you are unfamiliar with Requests, see below article:
--- diigging.py
+++ diigging.py
@@ -10,7 +10,7 @@ API_KEY = config('KEY')
 request_url = 'https://secure.diigo.com/api/v2/bookmarks'
 
 # Basic Query Returns 10 Bookmarks
-def basic_query():
+def query_basic():
     response = requests.get(
         request_url,
         params={
@@ -54,9 +54,42 @@ def query_multiple_pages():
         print(json_response)
         page_number +=1
 
+# Query Through All Pages
+def query_all_pages():
+    counter_per_page = 10
+    filter_options = 'all'
+    page_number = 1
+    response_404 = 0
+    while response_404 != 404:
+        all_response = requests.get(
+            request_url,
+            params={
+                'key': API_KEY,
+                'user': API_USER,
+                'count': counter_per_page,
+                'filter': filter_options,
+                'start': page_number,
+            },
+            headers={
+                'Authorization': API_AUTHORIZATION_HEADER,
+            }
+        )
+
+        json_response = all_response.json()
+        print(json_response)
+        page_number +=1
+        if all_response.status_code == 404:
+            response_404 = 404
+
+        # For testing, to avoid actually pulling all pages
+        # if page_number == 3:
+        #     break
 
 # Uncomment line below to run basic_query
-# basic_query()
+# query_basic()
 
 # Uncomment line below to run query_multiple_pages
-query_multiple_pages()
+# query_multiple_pages()
+
+# Uncomment line below to run query_all_pages
+query_all_pages() 
