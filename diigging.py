4f65d36a43b17126df7179af39e7e551b4a3ba80
Dave Mackey <dave@davemackey.net>
2020-12-08 18:48:30 +0000

    Add query_multiple_pages()

--- diigging.py
+++ diigging.py
@@ -27,22 +27,36 @@ def basic_query():
     json_response = response.json()
     print(json_response)
 
+
+
 # Query Through Multiple Pages
 def query_multiple_pages():
+    max_page_number = 10
+    counter_per_page = 10
+    filter_options = 'all'
     page_number = 1
-    response = requests.get(
-        request_url,
-        params={
-            'key': API_KEY,
-            'user': API_USER,
-            'count': 10,
-            'filter': 'all'
-        },
-        headers={
-            'Authorization': API_AUTHORIZATION_HEADER,
-        }
-    )
+    while page_number <= max_page_number:
+        multiple_response = requests.get(
+            request_url,
+            params={
+                'key': API_KEY,
+                'user': API_USER,
+                'count': counter_per_page,
+                'filter': filter_options,
+                'start': page_number,
+            },
+            headers={
+                'Authorization': API_AUTHORIZATION_HEADER,
+            }
+        )
+
+        json_response = multiple_response.json()
+        print(json_response)
+        page_number +=1
 
 
 # Uncomment line below to run basic_query
 # basic_query()
+
+# Uncomment line below to run query_multiple_pages
+query_multiple_pages()
